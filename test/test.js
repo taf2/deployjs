d4f760680de3dbb0b1f6e020be605782eec223a2
Todd Fisher <todd.fisher@gmail.com>
2011-07-16 10:20:28 -0400

    expose commands that allow for testing

--- test/test.js
+++ test/test.js
@@ -2,13 +2,15 @@
 process.env.NODE_ENV = "test";
 var vows = require('vows'),
     assert = require('assert'),
-    deploy = require(__dirname + '/../deploy').Deploy;
-
+    sinon  = require('sinon'),
+    deploy = require(__dirname + '/../lib/deployjs').Deploy;
+    stub = require(__dirname + '/../lib/deployjs').stub;
 
 vows.describe('Division by Zero').addBatch({
   'executes start command': {
     'calls commands': function() {
-      console.log(deploy);
+      stub('send', function() { console.log('called send'); });
+      deploy.setup.cmd();
     }
   }
 }).run();
